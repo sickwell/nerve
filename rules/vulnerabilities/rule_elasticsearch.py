a822d2338487b478987c6802f793291eefcd03f6
sickwell <32671944+sickwell@users.noreply.github.com>
2020-10-27 17:18:16 +0300

    Update rule_elasticsearch.py

--- rules/vulnerabilities/rule_elasticsearch.py
+++ rules/vulnerabilities/rule_elasticsearch.py
@@ -5,7 +5,7 @@ from core.parser  import ScanParser
 class Rule:
   def __init__(self):
     self.rule = 'VLN_77D7'
-    self.rule_severity = 2
+    self.rule_severity = 3
     self.rule_description = 'This rule checks for Open Elasticsearch instances'
     self.rule_confirm = 'Identified an open Elasticsearch'
     self.rule_details = ''
