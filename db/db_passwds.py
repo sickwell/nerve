3cbdc3f306167ecb69e9ccefe7aa7f41c036f441
sickwell <32671944+sickwell@users.noreply.github.com>
2020-10-27 17:40:13 +0300

    Update db_passwds.py

--- db/db_passwds.py
+++ db/db_passwds.py
@@ -1,2 +1,2 @@
 
-known_weak = ['root', 'toor', 'guest', 'cisco', 'test', 'test123456', '123456', 'querty', 'admin', 'administrator', 'qazwsx123']
+known_weak = ['root', 'toor', 'guest', 'cisco', 'test', 'test123456', '123456', 'querty', 'admin', 'administrator', 'qazwsx123', 'sa']
