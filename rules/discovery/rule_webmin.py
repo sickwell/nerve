837822cb7a105e3145d95c7a37c54e9fc417073a
sickwell <32671944+sickwell@users.noreply.github.com>
2020-11-04 17:36:26 +0300

    Update rule_webmin.py

--- rules/discovery/rule_webmin.py
+++ rules/discovery/rule_webmin.py
@@ -5,7 +5,7 @@ from core.parser  import ScanParser
 class Rule:
   def __init__(self):
     self.rule = 'DSC_OWA9'
-    self.rule_severity = 1
+    self.rule_severity = 2
     self.rule_description = 'This rule checks for the exposure of Webmin Panels'
     self.rule_confirm = 'Identified a Webmin Panel'
     self.rule_details = ''
