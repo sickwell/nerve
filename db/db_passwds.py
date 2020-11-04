218e4d9714e76a16057db0d4169cc3c15d4a545a
sickwell <32671944+sickwell@users.noreply.github.com>
2020-11-04 15:17:11 +0300

    Update db_passwds.py
    upgraded passwords

--- db/db_passwds.py
+++ db/db_passwds.py
@@ -1,2 +1,2 @@
 
-known_weak = ['root', 'toor', 'guest', 'cisco', 'test', 'test123456', '123456', 'querty', 'admin', 'administrator', 'qazwsx123', 'sa']
+known_weak = ['root', 'toor', 'guest', 'cisco', 'test', 'test123456', '123456', 'querty', 'admin', 'administrator', 'qazwsx123', 'sa', 'nsroot', 'calvin', 'juniper123', 'pfsense', 'ubnt']
